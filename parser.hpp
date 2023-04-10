35e38687242d028304af765247e73482d6c46b58
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-04-10 18:40:23 +0530

    Create parser.hpp

---
+++ parser.hpp
@@ -0,0 +1,394 @@
+#ifndef __parser__
+#define __parser__
+
+#include <unordered_map>
+#include <string>
+#include <functional>
+#include <vector>
+#include <fstream>
+#include <exception>
+#include <iostream>
+#include <boost/tokenizer.hpp>
+#include "requirements.hpp"
+
+struct MIPS_Architecture
+{
+    vector< struct Command* > parametric_commands;
+    vector<vector<int>> parametrs;
+    std:: unordered_map<string,vector<int>> indices;
+    std:: unordered_map<string,vector<string>> stage_names;
+	int registers[32] = {0}, PCcurr = 0, PCnext;
+	std::unordered_map<std::string, std::function<int(MIPS_Architecture &, std::string, std::string, std::string)>> instructions;
+	std::unordered_map<std::string, int> registerMap, address;
+	static const int MAX = (1 << 20);
+	int data[MAX >> 2] = {0};
+	std::unordered_map<int, int> memoryDelta;
+	std::vector<std::vector<std::string>> commands;
+	std::vector<int> commandCount;
+	enum exit_code
+	{
+		SUCCESS = 0,
+		INVALID_REGISTER,
+		INVALID_LABEL,
+		INVALID_ADDRESS,
+		SYNTAX_ERROR,
+		MEMORY_ERROR
+	};
+
+	// constructor to initialise the instruction set
+	MIPS_Architecture(std::ifstream &file, int question)
+	{
+		instructions = {{"add", &MIPS_Architecture::add}, {"sub", &MIPS_Architecture::sub}, {"mul", &MIPS_Architecture::mul}, {"beq", &MIPS_Architecture::beq}, {"bne", &MIPS_Architecture::bne}, {"slt", &MIPS_Architecture::slt}, {"j", &MIPS_Architecture::j}, {"lw", &MIPS_Architecture::lw}, {"sw", &MIPS_Architecture::sw}, {"addi", &MIPS_Architecture::addi}};
+		for (int i = 0; i < 32; ++i)
+			registerMap["$" + std::to_string(i)] = i;
+		registerMap["$zero"] = 0;
+		registerMap["$at"] = 1;
+		registerMap["$v0"] = 2;
+		registerMap["$v1"] = 3;
+		for (int i = 0; i < 4; ++i)
+			registerMap["$a" + std::to_string(i)] = i + 4;
+		for (int i = 0; i < 8; ++i)
+			registerMap["$t" + std::to_string(i)] = i + 8, registerMap["$s" + std::to_string(i)] = i + 16;
+		registerMap["$t8"] = 24;
+		registerMap["$t9"] = 25;
+		registerMap["$k0"] = 26;
+		registerMap["$k1"] = 27;
+		registerMap["$gp"] = 28;
+		registerMap["$sp"] = 29;
+		registerMap["$s8"] = 30;
+		registerMap["$ra"] = 31;
+
+        switch (question)
+        {
+        case 1:
+            stage_names={{"add", { "IF", "ID", "EX", "MEM", "WB" }}, {"sub", { "IF", "ID", "EX", "MEM", "WB" }}, {"mul", { "IF", "ID", "EX", "MEM", "WB" }}, {"beq", { "IF", "ID", "EX", "MEM", "WB" }}, {"bne", { "IF", "ID", "EX", "MEM", "WB" }}, {"slt", { "IF", "ID", "EX", "MEM", "WB" }}, {"j", { "IF", "ID", "EX", "MEM", "WB" }}, {"lw", { "IF", "ID", "EX", "MEM", "WB" }}, {"lw_offset", { "IF", "ID", "EX", "MEM", "WB" }}, {"sw", { "IF", "ID", "EX", "MEM", "WB" }}, {"sw_offset", { "IF", "ID", "EX", "MEM", "WB" }}, {"addi", { "IF", "ID", "EX", "MEM", "WB" }}};
+            parametrs={{1,20},{70,100,150,200,120}};
+            indices = {{"add", {2,2,4}}, {"sub", {2,2,4}}, {"mul", {2,2,4}}, {"beq", {2,2,-1}}, {"bne", {2,2,-1}}, {"slt", {2,2,4}}, {"j", {-1,-1,-1}}, {"lw", {3,3,4}}, {"lw_offset", {2,3,4}}, {"sw", {3,3,-1}}, {"sw_offset", {2,3,-1}}, {"addi", {2,2,4}}};
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            
+            break;
+        }
+		constructCommands(file);
+		commandCount.assign(commands.size(), 0);
+	}
+
+	// perform add operation
+	int add(std::string r1, std::string r2, std::string r3)
+	{
+		return op(r1, r2, r3, [&](int a, int b)
+				  { return a + b; });
+	}
+
+	// perform subtraction operation
+	int sub(std::string r1, std::string r2, std::string r3)
+	{
+		return op(r1, r2, r3, [&](int a, int b)
+				  { return a - b; });
+	}
+
+	// perform multiplication operation
+	int mul(std::string r1, std::string r2, std::string r3)
+	{
+		return op(r1, r2, r3, [&](int a, int b)
+				  { return a * b; });
+	}
+
+	// perform the binary operation
+	int op(std::string r1, std::string r2, std::string r3, std::function<int(int, int)> operation)
+	{
+		if (!checkRegisters({r1, r2, r3}) || registerMap[r1] == 0)
+			return 1;
+		registers[registerMap[r1]] = operation(registers[registerMap[r2]], registers[registerMap[r3]]);
+		PCnext = PCcurr + 1;
+		return 0;
+	}
+
+	// perform the beq operation
+	int beq(std::string r1, std::string r2, std::string label)
+	{
+		return bOP(r1, r2, label, [](int a, int b)
+				   { return a == b; });
+	}
+
+	// perform the bne operation
+	int bne(std::string r1, std::string r2, std::string label)
+	{
+		return bOP(r1, r2, label, [](int a, int b)
+				   { return a != b; });
+	}
+
+	// implements beq and bne by taking the comparator
+	int bOP(std::string r1, std::string r2, std::string label, std::function<bool(int, int)> comp)
+	{
+		if (!checkLabel(label))
+			return 4;
+		if (address.find(label) == address.end() || address[label] == -1)
+			return 2;
+		if (!checkRegisters({r1, r2}))
+			return 1;
+		PCnext = comp(registers[registerMap[r1]], registers[registerMap[r2]]) ? address[label] : PCcurr + 1;
+		return 0;
+	}
+
+	// implements slt operation
+	int slt(std::string r1, std::string r2, std::string r3)
+	{
+		if (!checkRegisters({r1, r2, r3}) || registerMap[r1] == 0)
+			return 1;
+		registers[registerMap[r1]] = registers[registerMap[r2]] < registers[registerMap[r3]];
+		PCnext = PCcurr + 1;
+		return 0;
+	}
+
+	// perform the jump operation
+	int j(std::string label, std::string unused1 = "", std::string unused2 = "")
+	{
+		if (!checkLabel(label))
+			return 4;
+		if (address.find(label) == address.end() || address[label] == -1)
+			return 2;
+		PCnext = address[label];
+		return 0;
+	}
+
+	// perform load word operation
+	int lw(std::string r, std::string location, std::string unused1 = "")
+	{
+		if (!checkRegister(r) || registerMap[r] == 0)
+			return 1;
+		int address = locateAddress(location);
+		if (address < 0)
+			return abs(address);
+		registers[registerMap[r]] = data[address];
+		PCnext = PCcurr + 1;
+		return 0;
+	}
+
+	// perform store word operation
+	int sw(std::string r, std::string location, std::string unused1 = "")
+	{
+		if (!checkRegister(r))
+			return 1;
+		int address = locateAddress(location);
+		if (address < 0)
+			return abs(address);
+		if (data[address] != registers[registerMap[r]])
+			memoryDelta[address] = registers[registerMap[r]];
+		data[address] = registers[registerMap[r]];
+		PCnext = PCcurr + 1;
+		return 0;
+	}
+
+	int locateAddress(std::string location)
+	{
+		if (location.back() == ')')
+		{
+			try
+			{
+				int lparen = location.find('('), offset = stoi(lparen == 0 ? "0" : location.substr(0, lparen));
+				std::string reg = location.substr(lparen + 1);
+				reg.pop_back();
+				if (!checkRegister(reg))
+					return -3;
+				int address = registers[registerMap[reg]] + offset;
+				if (address % 4 || address < int(4 * commands.size()) || address >= MAX)
+					return -3;
+				return address / 4;
+			}
+			catch (std::exception &e)
+			{
+				return -4;
+			}
+		}
+		try
+		{
+			int address = stoi(location);
+			if (address % 4 || address < int(4 * commands.size()) || address >= MAX)
+				return -3;
+			return address / 4;
+		}
+		catch (std::exception &e)
+		{
+			return -4;
+		}
+	}
+
+	// perform add immediate operation
+	int addi(std::string r1, std::string r2, std::string num)
+	{
+		if (!checkRegisters({r1, r2}) || registerMap[r1] == 0)
+			return 1;
+		try
+		{
+			registers[registerMap[r1]] = registers[registerMap[r2]] + stoi(num);
+			PCnext = PCcurr + 1;
+			return 0;
+		}
+		catch (std::exception &e)
+		{
+			return 4;
+		}
+	}
+
+	// checks if label is valid
+	inline bool checkLabel(std::string str)
+	{
+		return str.size() > 0 && isalpha(str[0]) && all_of(++str.begin(), str.end(), [](char c)
+														   { return (bool)isalnum(c); }) &&
+			   instructions.find(str) == instructions.end();
+	}
+
+	// checks if the register is a valid one
+	inline bool checkRegister(std::string r)
+	{
+		return registerMap.find(r) != registerMap.end();
+	}
+
+	// checks if all of the registers are valid or not
+	bool checkRegisters(std::vector<std::string> regs)
+	{
+		return std::all_of(regs.begin(), regs.end(), [&](std::string r)
+						   { return checkRegister(r); });
+	}
+
+	/*
+		handle all exit codes:
+		0: correct execution
+		1: register provided is incorrect
+		2: invalid label
+		3: unaligned or invalid address
+		4: syntax error
+		5: commands exceed memory limit
+	*/
+	void handleExit(exit_code code, int cycleCount)
+	{
+		std::cout << '\n';
+		switch (code)
+		{
+		case 1:
+			std::cerr << "Invalid register provided or syntax error in providing register\n";
+			break;
+		case 2:
+			std::cerr << "Label used not defined or defined too many times\n";
+			break;
+		case 3:
+			std::cerr << "Unaligned or invalid memory address specified\n";
+			break;
+		case 4:
+			std::cerr << "Syntax error encountered\n";
+			break;
+		case 5:
+			std::cerr << "Memory limit exceeded\n";
+			break;
+		default:
+			break;
+		}
+		if (code != 0)
+		{
+			std::cerr << "Error encountered at:\n";
+			for (auto &s : commands[PCcurr])
+				std::cerr << s << ' ';
+			std::cerr << '\n';
+		}
+	}
+
+    struct Command* define(std::vector<string> command){
+        bool in1=parametrs[0][0]==1;
+        int in2=parametrs[0][1];
+        vector<int> in3=parametrs[1];
+        vector<int> in4=indices[command[0]];
+        vector<int> in5;
+        switch(command.size()){
+            case 1:
+        }
+        vector<string> in6=stage_names[command[0]];
+        string in7=command[0];
+        int in9;
+        struct Command* cmd = new_Command(in1,in2,in3,in4,in5,in6,in7,0,in9);
+        return cmd;
+    }
+
+	// parse the command assuming correctly formatted MIPS instruction (or label)
+	void parseCommand(std::string line)
+	{
+		// strip until before the comment begins
+        int value=0;
+		line = line.substr(0, line.find('#'));
+		std::vector<std::string> command;
+		boost::tokenizer<boost::char_separator<char>> tokens(line, boost::char_separator<char>(", \t"));
+		for (auto &s : tokens)
+			command.push_back(s);
+		// empty line or a comment only line
+		if (command.empty())
+			return;
+		else if (command.size() == 1)
+		{
+			std::string label = command[0].back() == ':' ? command[0].substr(0, command[0].size() - 1) : "?";
+			if (address.find(label) == address.end())
+				address[label] = commands.size();
+			else
+				address[label] = -1;
+			command.clear();
+		}
+		else if (command[0].back() == ':')
+		{
+			std::string label = command[0].substr(0, command[0].size() - 1);
+			if (address.find(label) == address.end())
+				address[label] = commands.size();
+			else
+				address[label] = -1;
+			command = std::vector<std::string>(command.begin() + 1, command.end());
+		}
+		else if (command[0].find(':') != std::string::npos)
+		{
+			int idx = command[0].find(':');
+			std::string label = command[0].substr(0, idx);
+			if (address.find(label) == address.end())
+				address[label] = commands.size();
+			else
+				address[label] = -1;
+			command[0] = command[0].substr(idx + 1);
+		}
+		else if (command[1][0] == ':')
+		{
+			if (address.find(command[0]) == address.end())
+				address[command[0]] = commands.size();
+			else
+				address[command[0]] = -1;
+			command[1] = command[1].substr(1);
+			if (command[1] == "")
+				command.erase(command.begin(), command.begin() + 2);
+			else
+				command.erase(command.begin(), command.begin() + 1);
+		}
+		if (command.empty())
+			return;
+		if (command.size() > 4)
+			for (int i = 4; i < (int)command.size(); ++i)
+				command[3] += " " + command[i];
+		command.resize(4);
+		commands.push_back(command);
+        value=registers[registerMap[command[0]]];
+        struct Command* parametric= define(command);
+        parametric_commands.push_back(parametric);
+	}
+
+	// construct the commands vector from the input file
+	void constructCommands(std::ifstream &file)
+	{
+		std::string line;
+		while (getline(file, line))
+			parseCommand(line);
+		file.close();
+	}
+
+};
+
+#endif
