962e814003047a117452c840393af90cb694f5c5
SanyaMittal17 <97736276+SanyaMittal17@users.noreply.github.com>
2023-04-11 07:30:20 +0530

    Update branch_predictor.cpp

--- branch_predictor.cpp
+++ branch_predictor.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <fstream>
 using namespace std;
 
 string hex_to_binary(string s)
@@ -21,30 +22,40 @@ struct Data {
   vector<string> text;
 };
 
+vector<string> constructCommands(ifstream &file)
+	{
+		string line;
+        vector<string> l;
+		while (getline(file, line))
+			l.push_back(line);
+		file.close();
+    return l;
+	};
 
 int main(int start_state,string bhr_start,string bhr_start1){
   unordered_map<string,Data> our_map;
-//   assume l is a vector of strings having hex
 //   Predictions nikaalo sab cases me 
-  vector<string> l;
+  vector<string> l= constructCommands(branchtrace);
   for (int i=0;i<l.size();i++){
-    string str= hex_to_binary(l[i]);
-    l[i]=str.substr(str.size() - 15);
-     if (our_map.find(l[i]) != our_map.end()) {
-        our_map[l[i]].number=(our_map[l[i]].number+1)%4;
-        if (our_map[l[i]].number<2){
-            our_map[l[i]].text={our_map[l[i]].text[-1],"0"};
+    string x = l[i].substr(0, l[i].find(" "));
+    string str= hex_to_binary(x);
+    str = str.substr(str.size() - 15);
+     if (our_map.find(x) != our_map.end()) {
+        // this is not +1 check
+        our_map[x].number=(our_map[x].number+1)%4;
+        if (our_map[x].number<2){
+            our_map[x].text={our_map[x].text[-1],"0"};
         }
         else{
-            our_map[l[i]].text={our_map[l[i]].text[-1],"1"};
+            our_map[x].text={our_map[x].text[-1],"1"};
         }
     } else {
-        our_map[l[i]].number=(start_state+1)%4;
-        if (our_map[l[i]].number<2){
-            our_map[l[i]].text={bhr_start1,"0"};
+        our_map[x].number=(start_state+1)%4;
+        if (our_map[x].number<2){
+            our_map[x].text={bhr_start1,"0"};
         }
         else{
-            our_map[l[i]].text={bhr_start1,"1"};
+            our_map[x].text={bhr_start1,"1"};
         }}
     }
     return 0;
